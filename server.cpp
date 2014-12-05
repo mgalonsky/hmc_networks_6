e6139a86609a5115f77d908ee9574f9807a05803
HW_Seax <helen@Seax.(none)>
2014-12-04 20:51:18 -0800

    Started server.cpp

---
+++ server.cpp
@@ -0,0 +1,11 @@
+/*
+ * Helen Woodward and Melissa Galonsky
+ * Code for the servers for a simple distributed system
+ * for HMC CS125 lab 6
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <pthread.h>
+#include <sys/stat.h>
