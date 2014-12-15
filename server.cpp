b481cd70d914dbfb3be3f2ca9051cc090a8a5715
HW_Seax <helen@Seax.(none)>
2014-12-14 20:29:20 -0800

    created necessary structs and such and finished off init code

--- server.cpp
+++ server.cpp
@@ -11,14 +11,43 @@
 #include <pthread.h>
 #include <sys/stat.h>
 #include <map>
+#include <queue>
 
 using namespace std;
 
-map<SID, sockaddr_in> servermap;
-SID myID;
+struct serverMessage {
+	time_t time;
+	SID source;
+	bool isPing;
+	command ClientCommand;
+};
 
-void init(map<SID, sockaddr_in> serverMap, string configFile) {
+//comparison class for priority queue
+class CompareMessage {
+	public:
+	bool operator()(serverMessage& message1, serverMessage& message2) {
+		if(message1.time == message2.time) {
+			return message1.source < message2.source;
+		}
+		else {
+			return message1.time < message2.time;
+		}
+	}
+};
+
+typedef priority_queue<serverMessage, vector<serverMessage>, CompareMessage> message_pq;
+
+map<SID, sockaddr_in> serverMap;
+SID myID;
+int serverPort;
+int clientPort;
+time_t* timeTable;
+message_pq commandQueue;
+
+int init(string configFile) {
+	//set myID
 	myID = htonl(INADDR_ANY);
+	//set up serverMap
 	list<SID> serverList;
 	parseConfig(serverList, configFile);
 	sockaddr_in serveraddr;
@@ -28,9 +57,30 @@ void init(map<SID, sockaddr_in> serverMap, string configFile) {
 		serveraddr.sin_addr.s_addr = serverID;
 		serverMap.insert(pair<SID, sockaddr_in>(serverID, serveraddr));
 	}
+	//set up table of times
+		//variable size, so need it to be on the heap
+	timeTable = new time_t[serverMap.size()];
+	//don't need to do any extra pq set up because of typedef
+
+	//set up locks for priority queue
+	//set up signal for priority queue
+	//set up ports (both Server and Client)
+	if ((serverPort = setUpUdpSock(SPORT)) < 0) {
+		cerr << "error setting up server port" << endl;
+		return -1;
+	}
+	if ((clientPort = setUpUdpSock(CPORT)) < 0) {
+		cerr << "error setting up client port" << endl;
+		return -1;
+	}
+	return 0;
 }
 
 int main(int argc, char**argv) {
+	//call init for all set up
+	if (init("configFile") < 0) {
+		exit(1);
+	}
 }
 
 
