b784c50492f62de665fc6fe16d1caf0629b895c1
Melissa Galonsky <mgalonsky@cs.hmc.edu>
2014-12-14 21:46:36 -0800

    Wrote most of the client listener thread.

--- client_listener.cpp
+++ client_listener.cpp
@@ -1,1 +1,20 @@
 #include "client_listener.hpp"
+#include <ctime>
+
+void* listenToClients(void* args) {
+	command commandToRecieve;
+	serverMessage message;
+  	//Loop forever
+	for(;;) {
+		//Listens for an incoming command
+		recieveCommandFrom(CPORT, commandToRecieve);
+		
+		//Create a server mesasge
+		message.time = time(0);
+		message.source = myID;
+		message.isPing = false;
+		message.ClientCommand = commandToRecieve;
+		sendToServers(message);
+	}
+
+	
